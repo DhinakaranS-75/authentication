f68975d6cacd085eeb6d23ae13d64fb773c74d17
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-06-16 17:35:42 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,116 @@
+// index.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://0.0.0.0:27017/Login", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+// Create a user schema
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+});
+
+// Create a user model
+const User = mongoose.model('User', userSchema);
+
+// Register a new user
+app.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: 'Email already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+
+    return res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Failed to register user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Login with existing user
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    // Compare the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user._id }, 'dhinakaran_75', { expiresIn: '1h' });
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error('Failed to login:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Forgot password
+app.post('/forgot-password', async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Generate a new random password
+    const newPassword = Math.random().toString(36).slice(-8);
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update the user's password
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(200).json({ newPassword });
+  } catch (error) {
+    console.error('Failed to reset password:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
